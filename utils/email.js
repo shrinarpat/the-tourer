7c4aae3c78ab9e2547786feb73d42061a42a4b88
Narpat Singh Rawat <narpatsingh90918@gmail.com>
2023-08-27 23:48:31 +0530

    secure flag added

--- utils/email.js
+++ utils/email.js
@@ -19,6 +19,7 @@ module.exports = class Email {
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD,
